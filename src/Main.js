5968009573b01ef1916484a5215a8fdad1f061c4
teaganryley <teaganschultz@gmail.com>
2020-11-30 15:07:58 -0500

    autocomplete w/ collections

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AsyncAutoComplete from './components/AsyncAutoComplete';
 
 import API from './api';
 
 export default function Main() {
-  const [response, setResponse] = useState('No response yet');
+  const [characters, setCharacters] = useState([]);
   const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    API.get('/character')
+      .then(({ data }) => {
+        setCharacters(data.docs);
+        setLoading(false);
+      })
+      .catch((er) => console.error(er));
+  }, []);
 
   if (isLoading) return (<div>Loading...</div>);
   return (
     <React.Fragment>
       <p>
-        {response}
+        {console.log(characters)}
       </p>
-      <AsyncAutoComplete />
+      <AsyncAutoComplete characters={characters} />
     </React.Fragment>
   );
 }
